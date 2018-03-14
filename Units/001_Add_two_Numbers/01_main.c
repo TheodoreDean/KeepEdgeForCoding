7a3f4150b3dbff39783bca3cb4a460abeac5262a
TheodoreDean <395698026@qq.com>
2018-03-14 16:14:40 +0800

    update_001

--- Units/001_Add_two_Numbers/01_main.c
+++ Units/001_Add_two_Numbers/01_main.c
@@ -1,9 +1,24 @@
 /*
  * main.c
  *
- *  Created on: 2018Äê3ÔÂ7ÈÕ
+ *  Created on: 2018Ã„Ãª3Ã”Ã‚7ÃˆÃ•
  *      Author: Pengyizhan
  */
+
+/*
+ 
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+
+*/
+ 
 #include <stdio.h>
 #include <stdlib.h>
 

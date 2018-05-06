0be63231ccc5a05df42f39497dbd370201d38722
pengyizhan <pengyizhan@192.168.0.104>
2018-05-06 14:52:03 +0800

    update 006 palindrome Numbers

---
+++ Units/006_palindromeNumbers/palindromeNumber.h
@@ -0,0 +1,14 @@
+//
+//  palindromeNumber.h
+//  keepMyEdge
+//
+//  Created by pengyizhan on 2018/5/6.
+//  Copyright © 2018年 pengyizhan. All rights reserved.
+//
+
+#ifndef palindromeNumber_h
+#define palindromeNumber_h
+
+#include <stdio.h>
+int isPalindrome(int x) ;
+#endif /* palindromeNumber_h */

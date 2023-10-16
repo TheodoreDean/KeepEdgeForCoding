4f2c90b84b9be2626d61b35018ea8431527bff7a
theodreadean <164004843@qq.com>
2023-10-16 13:36:17 +0800

    create python-miio commands

---
+++ Units/028_localControlMIoTDevice/Readme.md
@@ -0,0 +1,18 @@
+### Get DeviceID（did） and token and control the Xiaomi  Iot device
+
+#### Get the token
+> token extractor
+
+
+#### Control the device
+```
+pip install python-miio
+
+miiocli --help
+
+```
+
+> Refer to the scripts in the root folder
+
+
+> The ip address (e.g., 192.168.31.73) and token is mandatory
---
+++ Units/028_localControlMIoTDevice/closePlug.sh
@@ -0,0 +1,7 @@
+
+# open
+#sudo miiocli device --ip 192.168.31.73 --token ed0ec8e98fd21edd701ee653713a293b raw_command set_properties "[{'did':'575059092','siid':2,'piid':1,'value':True}]"
+
+# close
+sudo miiocli device --ip 192.168.31.73 --token ed0ec8e98fd21edd701ee653713a293b raw_command set_properties "[{'did':'575059092','siid':2,'piid':1,'value':False}]"
+
